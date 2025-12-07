2c4e110a10b17d7772eac8f4092c69fabd017dc7
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2025-12-07 13:39:51 +0100

    Test_readPosition

---
+++ internal/game/packets/common_test.go
@@ -0,0 +1,18 @@
+package packets
+
+import (
+	"goTibia/internal/game/domain"
+	"goTibia/internal/protocol"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_readPosition(t *testing.T) {
+	input := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
+	pr := protocol.NewPacketReader(input[:])
+
+	position := readPosition(pr)
+
+	require.Equal(t, domain.Coordinate{X: 0x201, Y: 0x403, Z: 0x5}, position)
+}
