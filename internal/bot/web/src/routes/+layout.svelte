3aa23e6ffb32ed2ff9d4d225a2bf911e727703ff
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2025-12-28 15:17:42 +0100

    svelte UI

---
+++ internal/bot/web/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+    import { connect } from '$lib/botStore';
+
+    onMount(() => {
+        connect();
+    });
+</script>
+
+<nav style="padding: 1rem; background: #333; color: white;">
+    <a href="/stats" style="color: white; margin-right: 10px;">Character Stats</a>
+    <a href="/fishing" style="color: white;">Fishing</a>
+</nav>
+
+<slot />
+
